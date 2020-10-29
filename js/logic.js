baf9d4f647c6acdf5e956ee7477ee55325468ec0
Troy Dorman <troydorman@gmail.com>
2020-10-28 22:33:39 -0500

    added search history functionality

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 const apiKey = "25ab3c34a12d0c6508e3ac9bdd6bfe25";
 const myurl = "http://api.openweathermap.org/data/2.5/forecast?appid=";
 
-const city = "&q=Minneapolis";
+let searchHistory = [];
 
 
 $(document).ready(() => letsGo());
@@ -10,16 +10,42 @@ $(document).ready(() => letsGo());
 
 function letsGo() {
 
-    $.ajax({
-        url: myurl + apiKey + city,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    $('#search-button').on('click', function () {
 
+        searchACity(document.querySelector('#search-field').value);
 
     });
+}
 
+function searchACity(cityName) {
 
+    document.querySelector("#search-results").innerHTML = "";
+
+    $.ajax({
+        url: myurl + apiKey + "&q=" + cityName,
+        method: "GET"
+    }).then(function (response) {
+
+        // add the city name to the searchResults array
+
+        searchHistory.push(cityName);
+        console.log('value of searchHistory array: ' + searchHistory);
+
+        // this will keep the search history to max 8 results
+        if (searchHistory.length >= 9) {
+            searchHistory.shift();
+        }
+
+        // iterate over the search history array and print items to the ordered list
+
+        searchHistory.slice().reverse().forEach(function (name) {
+            let listItem = $('<li>').text(name).addClass("list-group-item");
+
+            $('#search-results').append(listItem);
+
+        });
+
+    });
 }
 
 

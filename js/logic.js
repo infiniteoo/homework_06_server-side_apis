eaff6fdc890758ad0b090ff4dbdc97bf1d82ab46
Troy Dorman <troydorman@gmail.com>
2020-10-31 00:01:05 -0500

    cleaning up some stuff - refactoring, adding comments, etc.

--- js/logic.js
+++ js/logic.js
@@ -15,7 +15,6 @@ function letsGo() {
     $('#search-button').on('click', function () {
 
         searchACity(document.querySelector('#search-field').value);
-
     });
 
     if ($('#search-results').length > 0) {
@@ -23,7 +22,6 @@ function letsGo() {
         $(document).on("click", ".list-group-item", function () {
 
             searchACity(this.textContent);
-
         });
     }
 }
@@ -31,7 +29,6 @@ function letsGo() {
 function kelvinToFarenheit(kelvin) {
 
     return (kelvin - 273.15) * 1.8 + 32;
-
 }
 
 function searchACity(cityName) {
@@ -63,15 +60,11 @@ function searchACity(cityName) {
         document.querySelector("#results-temperature").textContent = "Temperature: " +
             kelvinToFarenheit(parseFloat(response.list[0].main.temp)).toFixed(1) + "°F";
 
-
-        console.log(response);
-
         document.querySelector("#results-humidity").textContent = "Humidity: " + response.list[0].main.humidity + "%";
 
         document.querySelector("#results-wind-speed").textContent = "Wind Speed: " + response.list[0].wind.speed + " MPH";
 
         document.querySelector("#results-date").textContent = "\xa0\xa0(" + currentDate + ")";
-
 
         $("#results-icon").attr("src", picUrl);
 
@@ -103,6 +96,7 @@ function searchACity(cityName) {
     });
 
     function uvSearch(lat, lon) {
+
         $.ajax({
 
             url: myurl + `uvi?lat=${lat}&lon=${lon}&appid=` + apiKey,
@@ -110,8 +104,8 @@ function searchACity(cityName) {
 
         }).then(function (response) {
 
-
             $('#uvSpan').css("padding", "5px");
+
             if (response.value < 3) {
                 $('#uvSpan').css("background-color", "green");
             } else if (response.value > 2 && response.value < 6) {
@@ -119,6 +113,7 @@ function searchACity(cityName) {
             } else {
                 $('#uvSpan').css("background-color", "red");
             }
+
             document.querySelector("#uvSpan").textContent = response.value;
 
         });
@@ -126,16 +121,15 @@ function searchACity(cityName) {
 }
 
 function fiveDayForecast(list) {
+
     let numberOfDays = 2;
 
     list.forEach(function (i) {
-
 
         let iteratedDate = moment(i.dt_txt).format("MM DD YYYY");
         let checkDate = moment().add(numberOfDays, 'days').format("MM DD YYYY");
 
         if (checkDate === iteratedDate && numberOfDays < 7) {
-
 
             // use fiveDayArray to add each day's relevant info 
             // we need: date, icon, max_temp & humidity
@@ -164,22 +158,27 @@ function fiveDayForecast(list) {
 
 function buildFiveBoxes(box) {
 
-    console.log(box);
-
     box.forEach(function (index) {
-
+        // this box will hold all the info
         let newBox = $("<div>");
         newBox.addClass("fiveDayBox");
         newBox.text(index.date);
+
+        // build the weather icon picture
         let picUrl = "http://openweathermap.org/img/wn/" + index.icon + "@2x.png";
         let newPic = $("<img>").attr("src", picUrl);
         newBox.append(newPic);
+
+        // the temperature span 
         let tempSpan = $("<span>").text(kelvinToFarenheit(index.temp_max).toFixed(2) + "°F").css("text-align", "center").css("font-size", "40px").css("padding-bottom", "10px");
         newBox.append(tempSpan);
+
+        // the humidity span
         let humiditySpan = $("<span>").text("Humidity: " + index.humidity + "%").css("text-align", "center");
         newBox.append("<br>");
         newBox.append(humiditySpan);
 
+        // add the box to the UI
         $("#five-boxes").append(newBox);
 
     });
